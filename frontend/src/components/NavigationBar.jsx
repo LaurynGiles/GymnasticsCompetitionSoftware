4c9ed35e6c7f742292d35c6f760b5b5daf7b40da
Giles, LC, Miss [24699829@sun.ac.za] <24699829@sun.ac.za>
2024-05-13 23:42:28 +0200

    Building Judge Home Page

---
+++ frontend/src/components/NavigationBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+// import { RightHeader } from "./RightHeader";
+
+export const JudgeHeader = () => {
+  return (
+    <div className="flex items-center justify-center gap-[180px] px-[13px] py-[12px] relative bg-glaucous">
+      {/* <img className="relative flex-[0_0_auto]" alt="Left header" src="left-header.svg" />
+      <RightHeader className="!relative !flex-[0_0_auto]" /> */}
+    </div>
+  );
+};
--- frontend/src/pages/LoginJudges.jsx
+++ frontend/src/pages/LoginJudges.jsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 import BlueButton from "../components/BlueButton";
 import InputBox from "../components/InputBox";
@@ -10,10 +9,10 @@ const LoginJudges = () => {
       <div className="bg-glaucous overflow-hidden w-[400px] h-[800px]">
         <div className="relative w-[446px] h-[769px] top-[-162px] left-[-23px]">
           <div className="inline-flex flex-col items-center gap-[20px] px-[50px] py-[70px] absolute top-[500px] left-[34px]">
-            <InputBox />
-            <BlueButton />
+            <InputBox/>
+            <BlueButton/>
           </div>
-          <SmallLogo />
+          <SmallLogo/>
         </div>
       </div>
     </div>
@@ -21,31 +20,3 @@ const LoginJudges = () => {
 };
 
 export default LoginJudges;
-
-// import React from "react";
-
-// const LoginJudges = () => {
-//   return (
-//     <div className="flex justify-center items-center h-screen bg-glaucous">
-//       <div className="flex flex-col items-center gap-20 px-10 py-10 bg-white rounded-lg">
-//         <div className="bg-bright-white rounded-md p-3">
-//           <div className="text-center font-medium text-text text-lg">
-//             GSA number
-//           </div>
-//         </div>
-//         <button className="bg-prussian-blue rounded-md shadow-md px-6 py-3">
-//           <div className="font-medium text-white text-lg">Login</div>
-//         </button>
-//       </div>
-//       <div className="absolute bottom-0 right-0">
-//         <img
-//           className="w-40 h-40 object-cover"
-//           alt="Logo"
-//           src="logo.png"
-//         />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default LoginJudges;
